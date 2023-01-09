f854d6213dba11df94463336fde09334bb12df01
Juliana Meneguete <jumeneguete@gmail.com>
2023-01-09 19:59:58 -0300

    add main class

---
+++ src/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 2;
+        System.out.println("Tabela de multiplicação de " + num);
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + num * i);
+        }
+    }
+}
